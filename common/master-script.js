d3590400622f41a73c8fe2164e72774614ead4e9
LWA <99604708+lwa22@users.noreply.github.com>
2023-05-18 03:06:26 +0530

    Share Button Visibility Issue FIxed

--- common/master-script.js
+++ common/master-script.js
@@ -45,9 +45,9 @@ document.addEventListener("DOMContentLoaded", function () {
 //Sticky Ads Container
 let UlixAdsWidget_ = document.getElementById("_ulixAds");
 let UlixAdsCloseBtn_ = document.createElement("span");
-UlixAdsWidget_.appendChild(UlixAdsCloseBtn_);
 UlixAdsCloseBtn_.id = "_closeAdsWdgt";
 UlixAdsCloseBtn_.innerHTML=" × ";
+UlixAdsWidget_.appendChild(UlixAdsCloseBtn_);
 
 //let sticky_ads_Bottom = sticky_Style[0];
 //UlixAdsWidget_.style.bottom =sticky_ads_Bottom;
